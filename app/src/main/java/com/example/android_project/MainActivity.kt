1939dc40e3196a39928f588fd0347e99eaccc688
Idorio <insaider9218@gmail.com>
2022-11-24 22:07:26 +0300

    Lesson20

--- app/src/main/java/com/example/android_project/MainActivity.kt
+++ app/src/main/java/com/example/android_project/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.android_project
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.activity_containter,OnBoardingFragment())
+        fragmentTransaction.commit()
     }
 }
---
+++ app/src/main/java/com/example/android_project/OnBoardingFragment.kt
@@ -0,0 +1,32 @@
+package com.example.android_project
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+
+class OnBoardingFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_on_boarding, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val onBoardingFragment = view.findViewById<Button>(R.id.btnFinish)
+        onBoardingFragment.setOnClickListener{
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.activity_containter, ItemsFragment())
+                .commit()
+        }
+    }
+}
